a0eb96d9045d5305e4007df649ed77e913da3c84
Dane Summers <dsummersl@yahoo.com>
2016-11-15 19:10:36 -0500

    Center data and seed data on Durham.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@ r = Random.new
 500.times do |i|
   Thing.where(city_id: i).first_or_initialize.tap do |thing|
     thing.name = "Some Drain #{i}"
-    thing.lat = r.rand(37.75..37.78)
-    thing.lng = r.rand(-122.43..-122.41)
+    thing.lat = r.rand(35.90..36.10)
+    thing.lng = r.rand(-79.10..-78.90)
     thing.system_use_code = ['MS4', 'STORM', 'COMB', 'UNK'].sample
     thing.save!
   end
