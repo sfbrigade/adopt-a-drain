160a045ab4e993d49bfd4de7e4ab98c2cfb432b6
root <root@Adoptadrain.sfgov.org>
2017-05-02 15:26:41 -0700

    More controller tests

--- test/controllers/adopted_controller_test.rb
+++ test/controllers/adopted_controller_test.rb
@@ -4,6 +4,7 @@ class AdoptedControllerTest < ActionController::TestCase
   setup do
     request.env['devise.mapping'] = Devise.mappings[:user]
     @user = users(:erik)
+    @admin = users(:admin)
   end
 
   test 'should get index' do
@@ -11,5 +12,33 @@ class AdoptedControllerTest < ActionController::TestCase
 
     get :index
     assert_response :success
+  end
+
+  test 'should get json' do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(@admin.email, 'correct')
+
+    get :index, format: :json
+    assert_equal 'application/json', @response.content_type
+  end
+
+  test 'should get xml' do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(@admin.email, 'correct')
+
+    get :index, format: :xml
+    assert_equal 'application/xml', @response.content_type
+  end
+
+  test 'should get csv' do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(@admin.email, 'correct')
+
+    get :index, format: :csv
+    assert_equal 'text/csv', @response.content_type
+  end
+
+  test 'only admins get access' do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(@user.email, 'correct')
+
+    get :index
+    assert_equal 'text/html', @response.content_type # If user were an admin, content_type would be JSON, since that is default
   end
 end
