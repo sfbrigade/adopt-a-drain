d16cbb3dbb77460bb364b24d6d3c69e2f9a79936
root <root@Adoptadrain.sfgov.org>
2017-05-01 21:40:54 -0700

    Adopted Drains API controller test

--- test/controllers/adopted_controller_test.rb
+++ test/controllers/adopted_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class AdoptedControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    request.env['devise.mapping'] = Devise.mappings[:user]
+    @user = users(:erik)
+  end
+
+  test "should get index" do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(@user.email, "correct")
+
+    get :index 
+    assert_response :success
+  end
 end
