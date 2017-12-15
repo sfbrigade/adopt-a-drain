78c0c75945e38b093bcf1957543c7dc61e96f07b
James Fowler <james@Jamess-MacBook-Air.local>
2017-12-15 13:43:32 -0800

    temporarily changed asset host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   config.cache_classes = false
 
   # asset host
-  config.action_controller.asset_host = 'http://' + Socket.ip_address_list[1].ip_address + ':3000'
+  config.action_controller.asset_host = 'http://localhost:3000'
   config.action_mailer.asset_host = config.action_controller.asset_host
 
   # Do not eager load code on boot.
