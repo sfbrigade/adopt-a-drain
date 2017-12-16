ea959a897d6d3eaa84768d0794b4bdc2a879d4b8
James Fowler <james@Jamess-MacBook-Air.local>
2017-12-15 19:27:50 -0800

    Response data tests for pagination counts and drain data

--- test/controllers/adopted_controller_test.rb
+++ test/controllers/adopted_controller_test.rb
@@ -4,7 +4,15 @@ class AdoptedControllerTest < ActionController::TestCase
   setup do
     request.env['devise.mapping'] = Devise.mappings[:user]
     @user = users(:erik)
+    @user2 = users(:dan)
     @admin = users(:admin)
+    @thing = things(:thing_1)
+    @thing2 = things(:thing_2)
+
+    @thing.user_id = @user.id
+    @thing2.user_id = @user2.id
+    @thing.save
+    @thing2.save
   end
 
   test 'should get index' do
@@ -17,22 +25,8 @@ class AdoptedControllerTest < ActionController::TestCase
   test 'should get json' do
     @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(@admin.email, 'correct')
 
-    get :index, format: :json
+    get :index
     assert_equal 'application/json', @response.content_type
-  end
-
-  test 'should get xml' do
-    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(@admin.email, 'correct')
-
-    get :index, format: :xml
-    assert_equal 'application/xml', @response.content_type
-  end
-
-  test 'should get csv' do
-    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(@admin.email, 'correct')
-
-    get :index, format: :csv
-    assert_equal 'text/csv', @response.content_type
   end
 
   test 'only admins get access' do
@@ -40,5 +34,32 @@ class AdoptedControllerTest < ActionController::TestCase
 
     get :index
     assert_equal 'text/html', @response.content_type # If user were an admin, content_type would be JSON, since that is default
+  end
+
+
+  test 'drain data is correct' do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(@admin.email, 'correct')
+
+    get :index
+    random_drain = JSON.parse(@response.body)["drains"].first
+
+    drain = Thing.find_by(city_id: random_drain["city_id"].gsub("N-",""))
+
+    assert_not_nil drain
+    assert_equal drain.lat.to_s, random_drain["latitude"]
+    assert_equal drain.lng.to_s, random_drain["longitude"]
+
+  end
+
+  test 'page counts' do
+    @request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(@admin.email, 'correct')
+    
+    get :index
+    json = JSON.parse(@response.body)
+    puts(json)
+
+    assert_equal json["next_page"], 2
+    assert_equal json["prev_page"], -1
+    assert_equal json["total_pages"], 1
   end
 end
