3a65cd3fb03c968c6eeccdbc2e10086727ae4e6e
Jesse Szwedko <jesse.szwedko@gmail.com>
2018-05-15 10:11:33 -0700

    Run database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,83 +10,101 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123031630) do
+ActiveRecord::Schema.define(version: 2018_05_15_170343) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "cube"
   enable_extension "earthdistance"
+  enable_extension "plpgsql"
 
-  create_table "rails_admin_histories", force: :cascade do |t|
-    t.string   "message"
-    t.string   "username"
-    t.integer  "item"
-    t.string   "table"
-    t.integer  "month",      limit: 2
-    t.integer  "year",       limit: 8
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
-
-  create_table "reminders", force: :cascade do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "from_user_id",                 null: false
-    t.integer  "to_user_id",                   null: false
-    t.integer  "thing_id",                     null: false
-    t.boolean  "sent",         default: false
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  add_index "reminders", ["from_user_id"], name: "index_reminders_on_from_user_id", using: :btree
-  add_index "reminders", ["sent"], name: "index_reminders_on_sent", using: :btree
-  add_index "reminders", ["thing_id"], name: "index_reminders_on_thing_id", using: :btree
-  add_index "reminders", ["to_user_id"], name: "index_reminders_on_to_user_id", using: :btree
-
-  create_table "things", force: :cascade do |t|
+  create_table "rails_admin_histories", id: :serial, force: :cascade do |t|
+    t.string "message"
+    t.string "username"
+    t.integer "item"
+    t.string "table"
+    t.integer "month", limit: 2
+    t.bigint "year"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.decimal  "lat",             precision: 16, scale: 14, null: false
-    t.decimal  "lng",             precision: 17, scale: 14, null: false
-    t.integer  "city_id"
-    t.integer  "user_id"
-    t.string   "system_use_code"
+    t.index ["item", "table", "month", "year"], name: "index_rails_admin_histories"
+  end
+
+  create_table "reminders", id: :serial, force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "from_user_id", null: false
+    t.integer "to_user_id", null: false
+    t.integer "thing_id", null: false
+    t.boolean "sent", default: false
+    t.index ["from_user_id"], name: "index_reminders_on_from_user_id"
+    t.index ["sent"], name: "index_reminders_on_sent"
+    t.index ["thing_id"], name: "index_reminders_on_thing_id"
+    t.index ["to_user_id"], name: "index_reminders_on_to_user_id"
+  end
+
+  create_table "things", id: :serial, force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "name"
+    t.decimal "lat", precision: 16, scale: 14, null: false
+    t.decimal "lng", precision: 17, scale: 14, null: false
+    t.integer "city_id"
+    t.integer "user_id"
+    t.string "system_use_code"
     t.datetime "deleted_at"
-    t.string   "adopted_name"
+    t.string "adopted_name"
+    t.boolean "priority", default: false, null: false
+    t.index ["city_id"], name: "index_things_on_city_id", unique: true
+    t.index ["deleted_at"], name: "index_things_on_deleted_at"
   end
 
-  add_index "things", ["city_id"], name: "index_things_on_city_id", unique: true, using: :btree
-  add_index "things", ["deleted_at"], name: "index_things_on_deleted_at", using: :btree
-
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :serial, force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "organization"
-    t.string   "email",                                  null: false
-    t.string   "voice_number"
-    t.string   "sms_number"
-    t.string   "address_1"
-    t.string   "address_2"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zip"
-    t.boolean  "admin",                  default: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+    t.string "organization"
+    t.string "email", null: false
+    t.string "voice_number"
+    t.string "sms_number"
+    t.string "address_1"
+    t.string "address_2"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.boolean "admin", default: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "first_name"
+    t.string "last_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
