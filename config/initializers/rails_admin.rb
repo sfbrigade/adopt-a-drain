781d5daa9ba3510c5cb1f5e98694e431dccfe151
Jesse Szwedko <jesse.szwedko@gmail.com>
2018-05-15 10:00:27 -0700

    Exclude models without tables from Rails Admin
    See: https://github.com/sferik/rails_admin/issues/3014

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 RailsAdmin.config do |config|
+  # not real models
+  # https://github.com/sferik/rails_admin/issues/3014
+  config.excluded_models = ['ActiveStorage::Blob', 'ActiveStorage::Attachment']
+
   config.authenticate_with do
     redirect_to(main_app.root_path, flash: {warning: 'You must be signed-in as an administrator to access that page'}) unless signed_in? && current_user.admin?
   end
