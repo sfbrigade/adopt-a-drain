fcf44776f9853166948c2832dd927b8468e10639
Jesse Szwedko <jesse@szwedko.me>
2018-10-23 18:46:02 +0200

    Only set key geocoding API key if environment variable is set
    Neglected that .warn returns true.

--- config/initializers/geokit.rb
+++ config/initializers/geokit.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
-Geokit::Geocoders::GoogleGeocoder.api_key = ENV['GOOGLE_GEOCODER_API_KEY'] || Rails.logger.warn('$GOOGLE_GEOCODER_API_KEY not set')
+if ENV['GOOGLE_GEOCODER_API_KEY']
+  Geokit::Geocoders::GoogleGeocoder.api_key = ENV['GOOGLE_GEOCODER_API_KEY']
+else
+  Rails.logger.warn('$GOOGLE_GEOCODER_API_KEY not set')
+end
