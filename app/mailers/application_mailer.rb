616e8de7a997f704ae2eb16e3b00e9845116a5cf
Alex Ball <me@alexjball.com>
2022-05-03 19:03:20 -0400

    Fix mailer views

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'Adopt a Drain Mystic River <todo@example.com>'
   layout 'mailer'
+  helper CityHelper
 end
--- test/mailers/previews/thing_mailer_preview.rb
+++ test/mailers/previews/thing_mailer_preview.rb
@@ -2,4 +2,15 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/thing_mailer
 class ThingMailerPreview < ActionMailer::Preview
+  def first_adoption_confirmation
+    ThingMailer.first_adoption_confirmation(Thing.for_city('everett').where.not(user: nil).first)
+  end
+
+  def second_adoption_confirmation
+    ThingMailer.second_adoption_confirmation(Thing.for_city('everett').where.not(user: nil).first)
+  end
+
+  def third_adoption_confirmation
+    ThingMailer.third_adoption_confirmation(Thing.for_city('everett').where.not(user: nil).first)
+  end
 end
