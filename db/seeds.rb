1e2505e5fa537e2dc290c5c8ed1255eeddea9b62
Alex Ball <me@alexjball.com>
2022-06-06 16:21:48 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,14 +18,15 @@ end
 r = Random.new
 
 500.times do |i|
-  Thing.where(city_id: "seed-#{i}").first_or_initialize.tap do |thing|
+  Thing.with_deleted.where(city_id: "seed-#{i}").first_or_initialize.tap do |thing|
     thing.name = "Some Drain #{i}"
     thing.lat = r.rand(37.75..37.78)
     thing.lng = r.rand(-122.43..-122.41)
     thing.system_use_code = %w[MS4 STORM COMB UNK].sample
     thing.city_domain = 'everett'
-    thing.user_id = normal_user.id if i < 5
+    thing.user_id = i < 5 ? normal_user.id : nil
     thing.priority = [true, false].sample
+    thing.deleted_at = nil
     thing.save!
   end
 end
